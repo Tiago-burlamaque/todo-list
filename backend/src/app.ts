c56090a33c9de738a2134ccd387161c36539851f
Tiago-burlamaque <tiagoburlamaque79@gmail.com>
2026-07-27 17:38:36 -0300

    modificando tsconfig

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { authMiddleware } from './middleware/authMiddleware'
 import { usuarioRouter } from './routers/usuario.route'
@@ -8,6 +8,10 @@ export const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world!')
+})
 
 app.use('/auth', usuarioRouter)
 app.use('/tarefa', authMiddleware, tarefaRouter)
